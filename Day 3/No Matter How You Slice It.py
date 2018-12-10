b9873cd82cbdcaf89385360e90a7a0adbf379f77
Jarosław Ciołek-Żelechowski <zelechowski28@gmail.com>
2018-12-10 21:59:41 +0100

    I think that is it but I don't have internet to prove it! :smile:

--- Day 3/No Matter How You Slice It.py
+++ Day 3/No Matter How You Slice It.py
@@ -6,19 +6,17 @@ id_re = r'^#\d*'
 coord_re = r'\d*,\d*'
 area_re = r'\d*x\d*'
 
-size = 8
+size = 1000
 tab = [['.' for _ in range(size)] for _ in range(size)]
 
 
 def draw_rect(tab, id, coord, area):
 	for i in range(coord[0], coord[0] + area[0]):
 		for j in range(coord[1], coord[1] + area[1]):
-			print(f'I: {i}, J: {j}')
-			print(f'tab[i - 1]pj - 1]: {tab[i - 1][j - 1]}')
-			if tab[i - 1][j - 1].isdigit():
-				tab[i - 1][j - 1] = 'X'
-			else:
-				tab[i - 1][j - 1] = str(id)
+			if tab[i][j].isdigit():
+				tab[i][j] = 'X'
+			elif tab[i][j] is '.':
+				tab[i][j] = str(id)
 	return tab
 
 
@@ -33,9 +31,9 @@ for line in input_str:
 	coord_str = re.search(coord_re, line).group()
 	coord = [int(coord_str[coord_str.index(',') + 1:]), int(coord_str[:coord_str.index(',')])]
 	area_str = re.search(area_re, line).group()
-	area = [int(area_str[:area_str.index('x')]), int(area_str[area_str.index('x') + 1:])]
-	# print(f'Word: {line[0:-1]}, Id: {id}, Coord: {coord}, Area: {area}')
+	area = [int(area_str[area_str.index('x') + 1:]), int(area_str[:area_str.index('x')])]
 	tab = draw_rect(tab, id, coord, area)
 
-# print(tab)
 print(f'Part 1: {count_x(tab)}')
+
+# 86535 is too low :c
