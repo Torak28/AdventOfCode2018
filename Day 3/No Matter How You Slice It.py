c46b21336806c43fd3c54391ffb51bc4ce759140
Jarosław Ciołek-Żelechowski <zelechowski28@gmail.com>
2018-12-10 12:06:43 +0100

    Somethink is wrong with the list notation. Have to check that!

--- Day 3/No Matter How You Slice It.py
+++ Day 3/No Matter How You Slice It.py
@@ -6,12 +6,27 @@ id_re = r'^#\d*'
 coord_re = r'\d*,\d*'
 area_re = r'\d*x\d*'
 
-size = 10
+size = 8
 tab = [['.' for _ in range(size)] for _ in range(size)]
 
 
 def draw_rect(tab, id, coord, area):
-	tab
+	for i in range(coord[0], coord[0] + area[0]):
+		for j in range(coord[1], coord[1] + area[1]):
+			print(f'I: {i}, J: {j}')
+			print(f'tab[i - 1]pj - 1]: {tab[i - 1][j - 1]}')
+			if tab[i - 1][j - 1].isdigit():
+				tab[i - 1][j - 1] = 'X'
+			else:
+				tab[i - 1][j - 1] = str(id)
+	return tab
+
+
+def count_x(tab):
+	count = 0
+	for i in tab:
+		count += i.count('X')
+	return count
 
 for line in input_str:
 	id = int(re.search(id_re, line).group()[1:])
@@ -19,4 +34,8 @@ for line in input_str:
 	coord = [int(coord_str[coord_str.index(',') + 1:]), int(coord_str[:coord_str.index(',')])]
 	area_str = re.search(area_re, line).group()
 	area = [int(area_str[:area_str.index('x')]), int(area_str[area_str.index('x') + 1:])]
-	print(f'Word: {line[0:-1]}, Id: {id}, Coord: {coord}, Area: {area}')
+	# print(f'Word: {line[0:-1]}, Id: {id}, Coord: {coord}, Area: {area}')
+	tab = draw_rect(tab, id, coord, area)
+
+# print(tab)
+print(f'Part 1: {count_x(tab)}')
